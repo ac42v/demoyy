50a50cdd7e1f7eb7a5dcc49abbc39972f467011c
ac42v <ac42v@hotmail.com>
2020-07-02 11:48:51 +0800

    Initial commitkjugfyhghjbhji

---
+++ src/main/java/com/example/demoyy/DemoyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoyy/DemoyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
